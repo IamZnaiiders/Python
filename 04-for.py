f798762ea8f94889de6c87521d69e4e660ff4f50
IamZnaiiders <129349453+IamZnaiiders@users.noreply.github.com>
2023-04-04 13:18:53 -0600

    Ciclo infinito

--- 04-for.py
+++ 04-for.py
@@ -1,11 +1,22 @@
 """Ciclos FOR"""
 
 # Estructura de un for 
+#range crea una secuencia o un listado que va a iniciar desde el 0
+buscar=6
 for numero in range(5):
-    print(numero, numero * 'Hola mundo')
+    print(numero)
+    if numero ==buscar:
+        print('Encontrado => ',buscar)
+        break
+else:
+    print("No encontre el numero buscado")
 
-#Se agregaron nuevas lineas
-#hola esta es la nueva estructiura de un ciclo 
-#vamos a renombrar un github
-#vammos hacer un cambio
-#desde la consola de comandos
+    print("*"*10)
+
+    comando = ""
+
+    while comando.lower() != "salir":
+        comando = input("$ ")
+        print(comando)
+
+#loop infinito 
