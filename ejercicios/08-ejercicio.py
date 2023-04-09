98ac25be75bc62e6711b1131a263da1c6b38aee0
IamZnaiiders <129349453+IamZnaiiders@users.noreply.github.com>
2023-04-09 11:30:48 -0600

    Ejercicios agregados

---
+++ ejercicios/08-ejercicio.py
@@ -0,0 +1,10 @@
+"""Escribí un programa que solicite al usuario el ingreso de dos palabras, 
+las cuales se guardarán en dos variables distintas. A continuación, almacená 
+en una variable la concatenación de la primera palabra, más un espacio, más la 
+segunda palabra. Mostrá este resultado en pantalla."""
+
+word_1=str(input("Palabra 1 => "))
+word_2=str(input("Palabra 2 => "))
+
+print("Palabra 1 => " +word_1 + "Palabra 2 => "+ word_2)
+
